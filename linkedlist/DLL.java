512521489fb07a8811302683c99b00ada0abef9d
Prince Roy <princeku.ray143@gmail.com>
2022-03-25 00:17:52 +0530

    added insertFirst(int val) & display() func

--- linkedlist/DLL.java
+++ linkedlist/DLL.java
@@ -3,7 +3,6 @@ package linkedlist;
 public class DLL {
 
 	private Node head;
-	private Node tail;
 	private int size;
 	public DLL() { this.size = 0; }
 	
@@ -13,21 +12,37 @@ public class DLL {
 		node.prev = null;
 		if (head != null) {
 			head.prev = node;
-			return;
-			
+			head = node;	
 		}
 		head = node;
+		size++;
+		
+	}
+	
+	public void insertLast(int val) {
+		
+			
 		
 	}
 	
 	public void display() {
 		Node temp = head;
+		Node last = null; //for having last node to print in reverse
 		System.out.print("Head-> ");
 		while (temp != null) {
 			System.out.print(temp.value + " -> ");
+			last = temp;
 			temp = temp.next;
 		}
 		System.out.println("Tail");
+		
+		//print in reverse
+		System.out.print("Tail-> ");
+		while (last != null) {
+			System.out.print(last.value + " -> ");
+			last = last.prev;
+		}
+		System.out.println("Head");
 	}
 	
 	private class Node{
--- linkedlist/DoubleLinkedList.java
+++ linkedlist/DoubleLinkedList.java
@@ -6,6 +6,11 @@ public class DoubleLinkedList {
 		// TODO Auto-generated method stub
 		DLL list = new DLL();
 		list.insertFirst(23);
+		list.insertFirst(12);
+		list.insertFirst(1);
+		list.insertFirst(40);
+		list.insertFirst(10);
+		list.insertLast(50);
 		list.display();
 
 	}
