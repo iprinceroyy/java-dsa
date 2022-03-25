921563880e56cf92a43776edb9a8cf7523a0aa82
Prince Roy <princeku.ray143@gmail.com>
2022-03-25 12:09:22 +0530

    defined insert(int after, int val)

--- linkedlist/DLL.java
+++ linkedlist/DLL.java
@@ -5,7 +5,7 @@ public class DLL {
 	private Node head;
 	private int size;
 	public DLL() { this.size = 0; }
-	
+
 	public void insertFirst(int val) {
 		Node node = new Node(val);
 		node.next = head;
@@ -16,15 +16,15 @@ public class DLL {
 		}
 		head = node;
 		size++;
-		
+
 	}
-	
+
 	public void insertLast(int val) {
 		Node node = new Node(val);
 		Node last = head;
-		
+
 		node.next = null;
-		
+
 		if (head == null) {
 			node.prev = null;
 			node.next = head;
@@ -34,13 +34,43 @@ public class DLL {
 		while (last.next != null) {
 			last = last.next;
 		}
-		
+
 		last.next = node;
 		node.prev = last;
 		size++;
-		
+
 	}
-	
+
+	public void insert(int after, int val) {
+
+		Node p = find(after);
+
+		if (p == null) {
+			System.out.println("doesn't exist");
+			return;
+		}
+
+		Node node = new Node(val);
+		node.next = p.next;
+		p.next = node;
+		node.prev = p;
+		if (node.next != null)
+			node.next.prev = node;
+		
+		size++;
+	}
+
+	public Node find(int val) {
+		Node node = head;
+		while (node != null) {
+			if (node.value == val) {
+				return node;
+			}
+			node = node.next;
+		}
+		return null;
+	}
+
 	public void display() {
 		Node temp = head;
 		Node last = null; //for having last node to print in reverse
@@ -51,7 +81,7 @@ public class DLL {
 			temp = temp.next;
 		}
 		System.out.println("Tail");
-		
+
 		//print in reverse
 		System.out.print("Tail-> ");
 		while (last != null) {
@@ -60,15 +90,15 @@ public class DLL {
 		}
 		System.out.println("Head");
 	}
-	
+
 	private class Node{
-		
+
 		private int value;
 		private Node next;
 		private Node prev;
-		
+
 		public Node(int value) { this.value = value; }
-		
+
 		public Node(int value, Node prev, Node next) {
 			this.value = value;
 			this.prev = prev;
--- linkedlist/DoubleLinkedList.java
+++ linkedlist/DoubleLinkedList.java
@@ -11,6 +11,7 @@ public class DoubleLinkedList {
 		list.insertFirst(40);
 		list.insertFirst(10);
 		list.insertLast(50);
+		list.insert(1, 34);
 		list.display();
 
 	}
