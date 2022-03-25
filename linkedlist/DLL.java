e46192124c350621d311076cceaf5cdde5b15b86
Prince Roy <princeku.ray143@gmail.com>
2022-03-25 10:41:21 +0530

    defined insertLast(int val)

--- linkedlist/DLL.java
+++ linkedlist/DLL.java
@@ -20,8 +20,24 @@ public class DLL {
 	}
 	
 	public void insertLast(int val) {
+		Node node = new Node(val);
+		Node last = head;
 		
-			
+		node.next = null;
+		
+		if (head == null) {
+			node.prev = null;
+			node.next = head;
+			head = node;
+			return;
+		}
+		while (last.next != null) {
+			last = last.next;
+		}
+		
+		last.next = node;
+		node.prev = last;
+		size++;
 		
 	}
 	
